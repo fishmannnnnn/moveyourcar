af7ca82495a48c20dd450ade19a63cf291f72e30
Vlad <vlad.lemkov@yandex.ru>
2024-08-05 23:57:35 +0400

    first commit

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,8 @@
+import styles from './Hero.module.scss';
+import cn from 'classnames';
+const Hero = () => {
+  return (
+    <section className={cn(styles.heroSection)}>Hero</section>
+  )
+}
+export default Hero
