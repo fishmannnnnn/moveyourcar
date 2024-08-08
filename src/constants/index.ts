ae60a2ba48606846114fd11be422e11c41c3ad0b
Vlad <vlad.lemkov@yandex.ru>
2024-08-08 18:42:00 +0400

    chores: add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,12 @@
+export const LINKS = [
+    { title: 'Services', href: '#services', key: 'services' },
+    { title: 'Licenses', href: '#licenses', key: 'licenses' },
+    { title: 'How it works', href: '#how-it-works', key: 'how-it-works' },
+    { title: 'Cases', href: '#cases', key: 'cases' },
+    { title: 'Reviews', href: '#reviews', key: 'reviews' },
+    { title: 'FAQ', href: '#FAQ', key: 'faq' },
+]
+export const LOCALES = [
+    { language: 'English', iso: 'en' },
+    { language: 'Spanish', iso: 'es' },
+]
