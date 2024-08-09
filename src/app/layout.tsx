78c0a4afc21880045f6fb3005f013b260d007219
Vlad <vlad.lemkov@yandex.ru>
2024-08-09 19:19:47 +0400

    change css to scss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 import '@/styles/normalize.css';
-import './globals.css';
+import './globals.scss';
 
 const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700']});
 
