5dc99b7b453a315a8fc4231ca24fb0fba405d030
Vlad <vlad.lemkov@yandex.ru>
2024-08-09 19:21:15 +0400

    chores: update content

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,12 +1,22 @@
+'use client';
+import Image from 'next/image';
 import styles from './Hero.module.scss';
 import cn from 'classnames';
 import { roboto, robotoCondensed } from '@/styles/fonts';
 import Navbar from '@/components/navbar/Navbar';
-
+import ButtonWhite from '../buttons/buttonWhite/ButtonWhite';
+import dashLeft from '@/images/dashboard-left.webp';
+import dashRight from '@/images/dashboard-right.webp';
+import { useState } from 'react';
 const Hero = () => {
+	const [radioValue, setRadioValue] = useState('');
 	return (
 		<section className={styles.section}>
-			<Navbar />
+			<div className={styles.navbarWrapper}>
+				<Navbar />
+			</div>
+			<Image src={dashLeft} alt='' className={styles.dashLeft}></Image>
+			<Image src={dashRight} alt='' className={styles.dashRight}></Image>
 			<div className='container'>
 				<div className={styles.container}>
 					<h1
@@ -41,8 +51,41 @@ const Hero = () => {
 							/>
 						</div>
 						<div className={styles.transportOptions}>
-							<input type='radio' value='open' />
-							<input type='radio' value='enclosed' />
+							<label
+								htmlFor='open'
+								className={
+									radioValue == 'open'
+										? 'radio-label--checked'
+										: 'radio-label'
+								}>
+								<input
+									type='radio'
+									value='open'
+									id='open'
+									name='transport'
+									onChange={() => setRadioValue('open')}
+								/>
+								<span>Open transport</span>
+							</label>
+							<label
+								htmlFor='enclosed'
+								className={
+									radioValue == 'enclosed'
+										? 'radio-label--checked'
+										: 'radio-label'
+								}>
+								<input
+									type='radio'
+									value='enclosed'
+									id='enclosed'
+									name='transport'
+									onChange={() => setRadioValue('enclosed')}
+								/>
+								<span>Enclosed transport</span>
+							</label>
+						</div>
+						<div className={styles.submit}>
+							<ButtonWhite text='Vehicle details' />
 						</div>
 					</form>
 				</div>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,17 +1,31 @@
 import styles from './Navbar.module.scss';
+import { useRef, useState } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
+import { inter } from '@/fonts';
 import f from '@/icons/f.svg';
 import insta from '@/icons/insta.svg';
 import x from '@/icons/x.svg';
-import LanguageSelect from '../language-select/LanguageSelect';
-import { inter } from '@/fonts';
+import menu from '@/icons/menu.svg';
 import cn from 'classnames';
 import { LINKS } from '@/constants';
-import Link from 'next/link';
+import LanguageSelect from '../language-select/LanguageSelect';
 import ButtonTransparent from '../buttons/buttonTransparent/ButtonTransparent';
+
 const Navbar = () => {
+	const [menuVisible, setMenuVisible] = useState(true);
+	const navRef = useRef<HTMLElement>(null);
+	const showMenu = () => {
+		let navEl: HTMLElement | null = navRef.current;
+		if (navEl) {
+			console.log(navEl);
+			navEl.classList.toggle(styles.navMobile);
+		}
+		setMenuVisible(!menuVisible);
+	};
 	return (
 		<header className={styles.header}>
+			<div className={styles.background}></div>
 			<div className={styles.headerTop}>
 				<div className={styles.headerTopInner}>
 					<div className={styles.contacts}>
@@ -26,22 +40,44 @@ const Navbar = () => {
 								<Image src={x} alt='x' />
 							</a>
 						</div>
-						<div className={styles.number}>
+						<a className={styles.number} href='tel:+19372723744'>
 							<span>+</span>1 <span>[</span>937<span>]</span>{' '}
 							272-37-44
-						</div>
+						</a>
 					</div>
 
-					<div className={styles.languageSelect}>
-						<LanguageSelect />
-					</div>
+					<LanguageSelect />
 				</div>
 			</div>
 			<div className={styles.headerBottom}>
 				<div className={styles.headerBottomInner}>
 					<div className={cn(styles.logo, inter.className)}>LOGO</div>
-					<nav>
-						<ul className={styles.nav}>
+					<nav className={styles.nav} ref={navRef}>
+						<div className={styles.contactsMobile}>
+							<div className={styles.contacts}>
+								<div className={styles.socials}>
+									<a href='http://www.facebook.com'>
+										<Image src={f} alt='facebook' />
+									</a>
+									<a href='http://www.instagram.com'>
+										<Image src={insta} alt='instagram' />
+									</a>
+									<a href='http://www.x.com'>
+										<Image src={x} alt='x' />
+									</a>
+								</div>
+								<a
+									className={styles.number}
+									href='tel:+19372723744'>
+									<span>+</span>1 <span>[</span>937
+									<span>]</span> 272-37-44
+								</a>
+							</div>
+						<div className={styles.languageSelect}>
+							<LanguageSelect />
+						</div>
+						</div>
+						<ul>
 							{LINKS.map((link) => (
 								<li className={styles.navLink} key={link.key}>
 									<Link href={link.href}>{link.title}</Link>
@@ -49,7 +85,14 @@ const Navbar = () => {
 							))}
 						</ul>
 					</nav>
-					<ButtonTransparent text='Get a quote' />
+					<div className={styles.navRight}>
+						<ButtonTransparent text='Get a quote' />
+						<button
+							className={styles.menuButton}
+							onClick={() => showMenu()}>
+							<Image src={menu} alt='menu'></Image>
+						</button>
+					</div>
 				</div>
 			</div>
 		</header>
